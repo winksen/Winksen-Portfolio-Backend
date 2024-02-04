7c46505de23d98a810e4b8c9510381c8d5dd9013
Amine <aminetabia98@gmail.com>
2024-02-04 19:11:28 +0100

    Update V0.1.12
    - Added ChangeLog migration, model and seeder
    - Configured ChangeLog migration

---
+++ database/migrations/2024_02_04_172558_create_changelogs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('changelogs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('type')->nullable();
+            $table->string('name')->nullable();
+            $table->string('href')->nullable();
+            $table->string('version')->nullable();
+            $table->string('comment')->nullable();
+            $table->string('date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('changelogs');
+    }
+};
