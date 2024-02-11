d9a3ce5715fcf3d1979803800bb45a7d9acb835f
Amine <aminetabia98@gmail.com>
2024-02-11 18:29:28 +0100

    Update V0.1.17
    - Added seeder for Blogs

--- database/seeders/BlogsTableSeeder.php
+++ database/seeders/BlogsTableSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Blog;
+use App\Models\Content;
 
-class BlogsTableSeeder extends Seeder
+class BlogSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        // Create sample blogs
+        $blogs = Blog::factory()->count(5)->create();
+
+        // Create content for each blog
+        $blogs->each(function ($blog) {
+            $contents = [
+                // Define content for each blog here, for example:
+                ['type' => 'paragraph', 'content' => ['Sample paragraph content']],
+                ['type' => 'quote', 'content' => 'Sample quote content'],
+                // Add more content sections as needed
+            ];
+
+            foreach ($contents as $contentData) {
+                $blog->contents()->create([
+                    'type' => $contentData['type'],
+                    'content' => json_encode($contentData),
+                ]);
+            }
+        });
     }
 }
