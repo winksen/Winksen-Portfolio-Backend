61a2f7410a5124fe9d66bb15a87cb4510c0be916
Amine <aminetabia98@gmail.com>
2024-04-11 20:04:32 +0000

    Update V0.1.68
    - Added necessary attributes to Migration and Model

--- app/Models/Identity.php
+++ app/Models/Identity.php
@@ -4,8 +4,55 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Identity extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'logo',
+        'logoMark',
+        'logoSecondary1',
+        'logoMarkSecondary1',
+        'logoSecondary2',
+        'logoMarkSecondary2',
+        'logoSecondary3',
+        'logoMarkSecondary3',
+        'logoSecondary4',
+        'logoMarkSecondary4',
+        'logoBW',
+        'logoMarkBW',
+        'typography',
+        'typographyImage',
+        'mockup1',
+        'mockup2',
+        'mockup3',
+        'mockup4',
+        'mockup5',
+        'mockup6',
+        'mockup7',
+        'title',
+        'date',
+        'description',
+        'isNew',
+        'isHot',
+        'isFeatured',
+        'tag1',
+        'tag2',
+        'tag3',
+        'tag4',
+        'tag5',
+        'tag6',
+        'tag7',
+        'tag8',
+        'tag9',
+        'tag10',
+    ];
 }
--- database/migrations/2024_04_11_001731_create_identities_table.php
+++ database/migrations/2024_04_11_001731_create_identities_table.php
@@ -14,6 +14,43 @@ return new class extends Migration
         Schema::create('identities', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('logo')->nullable();
+            $table->string('logoMark')->nullable();
+            $table->string('logoSecondary1')->nullable();
+            $table->string('logoMarkSecondary1')->nullable();
+            $table->string('logoSecondary2')->nullable();
+            $table->string('logoMarkSecondary2')->nullable();
+            $table->string('logoSecondary3')->nullable();
+            $table->string('logoMarkSecondary3')->nullable();
+            $table->string('logoSecondary4')->nullable();
+            $table->string('logoMarkSecondary4')->nullable();
+            $table->string('logoBW')->nullable();
+            $table->string('logoMarkBW')->nullable();
+            $table->string('typography')->nullable();
+            $table->string('typographyImage')->nullable();
+            $table->string('mockup1')->nullable();
+            $table->string('mockup2')->nullable();
+            $table->string('mockup3')->nullable();
+            $table->string('mockup4')->nullable();
+            $table->string('mockup5')->nullable();
+            $table->string('mockup6')->nullable();
+            $table->string('mockup7')->nullable();
+            $table->string('title')->nullable();
+            $table->datetime('date')->nullable();
+            $table->longText('description')->nullable();
+            $table->boolean('isNew')->default(0);
+            $table->boolean('isHot')->default(0);
+            $table->boolean('isFeatured')->default(0);
+            $table->boolean('tag1')->default(0);
+            $table->boolean('tag2')->default(0);
+            $table->boolean('tag3')->default(0);
+            $table->boolean('tag4')->default(0);
+            $table->boolean('tag5')->default(0);
+            $table->boolean('tag6')->default(0);
+            $table->boolean('tag7')->default(0);
+            $table->boolean('tag8')->default(0);
+            $table->boolean('tag9')->default(0);
+            $table->boolean('tag10')->default(0);
         });
     }
 
